b6787ecd55f89be214f218266b04e118a7273da8
unsukatapl <unsukatapl@gmail.com>
2020-03-24 11:29:53 +0100

    shouldReturnZeroForEmpty - implemented and green test

--- src/main/java/tdd/day2/StringCalculator.java
+++ src/main/java/tdd/day2/StringCalculator.java
@@ -1,4 +1,10 @@
 package tdd.day2;
 
 public class StringCalculator {
+
+    public static final String RESULT_ZERO = "0";
+
+    public String add(String number) {
+        return RESULT_ZERO;
+    }
 }
--- src/test/java/tdd/day2/StringCalculatorTest.java
+++ src/test/java/tdd/day2/StringCalculatorTest.java
@@ -1,11 +1,20 @@
 package tdd.day2;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class StringCalculatorTest {
 
     @Test
     void shouldReturnZeroForEmpty() {
+        //given
         StringCalculator calc = new StringCalculator();
+        String empty = "";
+
+        //when
+        String result = calc.add(empty);
+
+        //then
+        Assertions.assertEquals("", empty);
     }
 }
